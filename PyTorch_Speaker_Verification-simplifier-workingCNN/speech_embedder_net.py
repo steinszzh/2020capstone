9fc3438aec38552f71f6a12aee878d1e4ff41c9e
Zhihong Zhang <goodzzh222@hotmail.com>
2020-12-21 07:02:49 -0500

    update for final

---
+++ PyTorch_Speaker_Verification-simplifier-workingCNN/speech_embedder_net.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  5 20:58:34 2018
+
+@author: harry
+"""
+
+import torch
+import torch.nn as nn
+
+from hparam import hparam as hp
+from utils import get_centroids, get_cossim, calc_loss
+
+class SpeechEmbedder(nn.Module):
+    
+    def __init__(self):
+        super(SpeechEmbedder, self).__init__()    
+        self.LSTM_stack = nn.LSTM(hp.data.nmels, hp.model.hidden, num_layers=hp.model.num_layer, batch_first=True)
+        for name, param in self.LSTM_stack.named_parameters():
+          if 'bias' in name:
+             nn.init.constant_(param, 0.0)
+          elif 'weight' in name:
+             nn.init.xavier_normal_(param)
+        self.projection = nn.Linear(hp.model.hidden, hp.model.proj)
+        
+    def forward(self, x):
+        x, _ = self.LSTM_stack(x.float()) #(batch, frames, n_mels)
+        #only use last frame
+        x = x[:,x.size(1)-1]
+        x = self.projection(x.float())
+        x = x / torch.norm(x, dim=1).unsqueeze(1)
+        return x
+
+class GE2ELoss(nn.Module):
+    
+    def __init__(self, device):
+        super(GE2ELoss, self).__init__()
+        self.w = nn.Parameter(torch.tensor(10.0).to(device), requires_grad=True)
+        self.b = nn.Parameter(torch.tensor(-5.0).to(device), requires_grad=True)
+        self.device = device
+        
+    def forward(self, embeddings):
+        torch.clamp(self.w, 1e-6)
+        centroids = get_centroids(embeddings)
+        cossim = get_cossim(embeddings, centroids)
+        sim_matrix = self.w*cossim.to(self.device) + self.b
+        loss, _ = calc_loss(sim_matrix)
+        return loss
+
+    # Convolutional neural network (two convolutional layers)
+class ConvNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(ConvNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc = nn.Linear(7*7*32, num_classes)
+        
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        return out
+    
+  
